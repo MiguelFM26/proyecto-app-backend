520be2f849b957fd18dacf814ce0865b1b955751
MiguelFM26 <killerexpert26@gmail.com>
2026-06-09 18:57:20 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2'); // Asegúrate de usar mysql2 para soportar MySQL 8+
 
 const db = mysql.createConnection({
-  host: 'mysql-1039d17b-b-killerexpert26-d18a.a.aivencloud.com',
+  host: 'mysql-1039d17b-killerexpert26-d18a.a.aivencloud.com',
   port: 11112,
   user: 'avnadmin',
   password: 'AVNS_Vbe8jeJJkm7I26scD7R',
-  database: 'defaultdb',
+  database: 'defaultdb', // ✅ Corregido de 'defaultdh' a 'defaultdb'
   ssl: {
-    rejectUnauthorized: false // Obligatorio para que Aiven acepte la conexión segura SSL
+    rejectUnauthorized: false
   }
 });
 
